9d7633c98d525546007d64b3ec3ee4003031b9ff
satyam-kumar-21 <109452939+satyam-kumar-21@users.noreply.github.com>
2023-10-22 03:46:07 +0530

    Add files via upload

---
+++ javascript/01.js
@@ -0,0 +1,114 @@
+// var let const 
+// var a;
+// a = 46;
+// var num1 = 10;
+// let num2 = 20;
+// const num3 = 30;
+
+// console.log(num1);
+// num1 = 12;
+// console.log(num1);
+// console.log(num2);
+// num2 = 24;
+// console.log(num2);
+
+// num3 = 35;
+// console.log(num3);
+
+// if(2 > 1){
+//     var b = 20;
+// }
+
+// if(2 > 1){
+//     let b = 20;
+//     console.log(b);
+// }
+
+// console.log(b);
+
+
+// data types 
+
+// comment 
+
+/*
+asdfgh r hjhg
+dfgbhnbgfd
+htgrfedwsdefrgth
+*/ 
+
+// 1. primitive data types 
+// String , Number, Boolean, undefined, null 
+// let name = "Satyam kumar"
+// let add = 'Sitamarhi bihar'
+// let lastName = `Thakur`
+
+// let age = 22
+// console.log("mera naam satyam hai or age ",age);
+// console.log(`mera naam satyam hai or age ${age}`);
+// console.log(typeof name);
+
+// let a = 20;
+// let b = 8767;
+// console.log(typeof a);
+
+// let sahi = true;
+// let galat = false
+
+// console.log(typeof sahi);
+
+
+// let kuchhBhi;
+// console.log(typeof kuchhBhi);
+
+// let nullValue = null;
+// console.log(typeof nullValue);
+
+// console.log(name);
+
+
+// 2. non-primitive data types or reference data types 
+// Array, Object, Function 
+
+// Conversion operation 
+
+// let a = "Satyam";
+// let b = Number(a)
+
+// console.log(typeof b);
+// console.log(b);
+
+// let num = "33"
+// let num2 = Number(num)
+// console.log(num2);
+
+// "33" => 33
+// "3abc" => NaN
+
+// console.log("1" + " Satyam");
+// console.log("1" + 2);
+// console.log("1"+ 2+ 3);
+
+// console.log(1 + "1");
+// console.log(1+2+"3");
+
+// let name = "Satyam"
+// let conName = Boolean(name);
+
+// Operator 
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(10**3);
+// console.log(2/2);
+// console.log(2%2); 
+
+// console.log(10 >= 10);
+// <, <= , > ,>=
+
+// let a = 22;
+// let b = "22"
+
+// console.log(a == b);
+// console.log(a === b);
---
+++ javascript/02.js
@@ -0,0 +1,62 @@
+// let str = "Satyam kumar"
+// let str1 = 'Sitamarhi'
+// let str3 = `sdfghb sh `
+
+// let name = new String("Mai satyam hu")
+
+// console.log(name);
+
+// let str = "mai javascript shikh rha hu";
+// console.log(str);
+// console.log(str.length);
+// console.log(str.toUpperCase());
+
+// let name1 = str.toUpperCase();
+// console.log(name1);
+
+// console.log(str.charAt(4));
+// console.log(str.indexOf('i'));
+
+// const str = '         mai javascript shikh rha hu     ';
+// console.log(str);
+// // const newstr = str.trim()
+// console.log(str.trim());
+
+
+// console.log(str.substring(5,15));
+
+// let str = "mai javascripTTTTTTT shikh rha hu";
+
+// console.log(str.replace('sh','py'));
+// console.log(str.includes("javascriptasdfg"));
+// console.log(str.toLowerCase());
+
+// let a = 1232;
+
+// let num = new Number(1234);
+// console.log(typeof a);
+
+// let num = 12345123;
+// console.log(typeof num);
+// let strNum = num.toString()
+// console.log(typeof strNum);
+// console.log(strNum.length);
+// console.log(strNum.includes(4));
+
+// console.log(num.toLocaleString('en-IN'));
+
+// Math 
+
+// console.log(Math);
+// console.log(Math.abs(4.2));
+// console.log(Math.round(6.6));
+// console.log(Math.ceil(2.7));
+// console.log(Math.floor(4.9));
+
+// console.log(Math.min(12,32,1,342,23,54));
+// console.log(Math.max(12,32,1,342,23,54));
+
+// console.log(Math.random());
+// console.log(Math.floor(Math.random() * 10));
+
+// console.log(Math.);
